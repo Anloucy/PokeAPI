5b0ac175e2e2741082dc1ba0bcdcfc58d48d3a80
Anloucy <anloucy@gmail.com>
2024-06-24 20:42:01 -0300

    Aprimoramentos no Css e Barra de pesquisa(Incompleta)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const main = document.querySelector("main")
 const root = document.querySelector(":root")
+const input = document.querySelector('input')
 
 function createPokemonCard(pokemon){
     const card = document.createElement('div')
@@ -16,8 +17,13 @@ function createPokemonCard(pokemon){
 
     card.append(img, name)
     document.querySelector('#pokemons').append(card)
-
+    
+    card.addEventListener("click", function (ev){
+        ev.target
+    })
 }
+
+const list = []
 
 async function GetPokemon(){
     const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
@@ -26,11 +32,38 @@ async function GetPokemon(){
     data.results.forEach(async (pokemon) => {
         const pokemonResponse = await fetch(pokemon.url)
         const pokemonData = await pokemonResponse.json()
+        list.push(pokemon)
         createPokemonCard(pokemonData)
     })
 }
 
 GetPokemon()
+
+
+input.addEventListener('input', function (){
+    let userInput = input.value.toLowerCase()
+
+    const filtered = list.filter(pokemon => pokemon.name.toLowerCase().includes(userInput))
+
+    const suges = filtered.map(pokemon => pokemon.name)
+    const sugestions = document.getElementById("sugestions")
+    
+    sugestions.innerHTML = "";
+
+    suges.forEach(sugestao => {
+        const listItem = document.createElement("li");
+        listItem.textContent = sugestao;
+        sugestions.appendChild(listItem);
+    });
+    input.addEventListener("blur", function() {
+        sugestions.innerHTML = "";
+        input.textContent = "";
+    });
+});
+
+
+
+
 
 document.getElementById("themeSwitcher").addEventListener('click', function() {
     if(main.dataset.theme === "dark"){
@@ -44,7 +77,6 @@ document.getElementById("themeSwitcher").addEventListener('click', function() {
         root.style.setProperty("--bk-color", "#fffffff")
         root.style.setProperty("--fk-color", "#000000")
 
-        
         main.dataset.theme ="dark"
     }
 })
